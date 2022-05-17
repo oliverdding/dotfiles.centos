14d66d45703abf93bc304f2bb24a31211c1b1105
oliverdding <oliverdding@outlook.com>
2022-05-17 17:25:16 +0800

    update

--- .config/fish/conf.d/30-environment.fish
+++ .config/fish/conf.d/30-environment.fish
@@ -1,3 +1,4 @@
+set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 set -gx GOPATH "$HOME/.local/share/go"
 set -gx GPG_TTY (tty)
 set -gx KUBECONFIG (echo (ls ~/.kube/config.d/* 2>/dev/null) | sed 's/ /:/g')
